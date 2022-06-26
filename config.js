9181f317d7cdb9cc5f32c21291ddcefa166ec761
habiba47 <79235266+habiba47@users.noreply.github.com>
2022-06-26 16:44:26 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+ const firebaseConfig = {
+        apiKey: "AIzaSyBO56U-3ZfXbrGOLREkBkuQ6tGPFG5dscs",
+        authDomain: "voting-app-f7698.firebaseapp.com",
+        projectId: "voting-app-f7698",
+        storageBucket: "voting-app-f7698.appspot.com",
+        messagingSenderId: "757077991869",
+        appId: "1:757077991869:web:e59f67c4f38cdaa12af2d4"
+      };
+    
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
